edbccf8a5bcf0daa9bac42d5e65828fc436a4231
Chris Weibel <christopher.weibel@gmail.com>
2022-12-29 14:36:14 -0500

    Update main.tf
    Update private cidr ingress

--- main.tf
+++ main.tf
@@ -14,18 +14,21 @@ resource "aws_security_group" "ocfp_mgmt_bosh_sg" {
   name          = "mgmt-bosh-sg"
   description   = "Inbound & outbound INTERNAL traffic for Management BOSH & Deployments"
   vpc_id        = var.vpc_id
-  # MVP Subnets
+  
+  revoke_rules_on_delete = true
+  
+  # Env Subnets
   ingress {
     from_port   = 0
     protocol    = "-1"
     to_port     = 0
     cidr_blocks = var.env_cidrs
   }
-  egress {
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
-    cidr_blocks = var.env_cidrs
+  ingress {
+    from_port   = 443
+    protocol    = "TCP"
+    to_port     = 443
+    cidr_blocks = var.private_cidrs
   }
   # AWS S3
   ingress {
@@ -34,16 +37,10 @@ resource "aws_security_group" "ocfp_mgmt_bosh_sg" {
     to_port     = 443
     cidr_blocks = var.aws_s3_cidrs
   }
-  egress {
-    from_port   = 443
-    protocol    = "TCP"
-    to_port     = 443
-    cidr_blocks = var.aws_s3_cidrs
-  }
+
   tags          = merge({Name = "mgmt-bosh-sg"}, var.resource_tags )
 }
 
-# formerly ocfp_aws_useast1_mgmt_bosh_sg
 
 output "ocfp_mgmt_bosh_sg_id" {
     value = "${aws_security_group.ocfp_mgmt_bosh_sg.id}"
@@ -59,32 +56,17 @@ resource "aws_security_group" "ocfp_mgmt_bastion_sg" {
   name          = "mgmt-bastion-sg"
   description   = "Inbound & outbound INTERNAL traffic for Bastion"
   vpc_id        = var.vpc_id
-  # MVP Subnets
+
+  # Env Subnets
   ingress {
     from_port   = 22
     protocol    = "TCP"
     to_port     = 22
     cidr_blocks = var.private_cidrs
   }
-  ingress {
-    from_port   = 6868
-    protocol    = "TCP"
-    to_port     = 6868
-    cidr_blocks = var.env_cidrs
-  }
-  ingress {
-    from_port   = 25555
-    protocol    = "TCP"
-    to_port     = 25555
-    cidr_blocks = var.env_cidrs
-  }
-  egress {
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
-    cidr_blocks = var.private_cidrs
-  }
+
   tags          = merge({Name = "mgmt-bastion-sg"}, var.resource_tags )
+
 }
 
 output "ocfp_mgmt_bastion_sg_id" {
